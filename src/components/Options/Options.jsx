700872d7960738a386bf80d0e1f62b27f8e525ea
lika_vil <125767690+Lika060@users.noreply.github.com>
2025-04-06 19:10:36 +0300

    Changes to be committed: 	modified:   src/components/Description/Description.module.css 	modified:   src/components/Feedback/Feedback.module.css 	modified:   src/components/Notification/Notification.module.css 	modified:   src/components/Options/Options.jsx 	modified:   src/components/Options/Options.module.css

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,64 +1,40 @@
-// import css from './Options.module.css';
+import css from './Options.module.css';
 
-// function Option({ updateFeedback, resetScore }) {
-//   return (
-//     <>
-//       <div className='mt-4'>
-//         <button
-//           onClick={() => updateFeedback("good")}
-//           className='btn btn-outline-dark px-3 py-2 m-2'
-//           style={{ border: "2px solid black" }}
-//         >
-//           Good
-//         </button>
-//         <button
-//           onClick={() => updateFeedback("neutral")}
-//           className='btn btn-outline-dark px-3 py-2 m-2'
-//           style={{ border: "2px solid black" }}
-//         >
-//           Neutral
-//         </button>
-//         <button
-//           onClick={() => updateFeedback("bad")}
-//           className='btn btn-outline-dark px-3 py-2 m-2'
-//           style={{ border: "2px solid black" }}
-//         >
-//           Bad
-//         </button>
-//         <button
-//           onClick={resetScore}
-//           className='btn btn-outline-dark px-3 py-2 m-2'
-//           style={{ border: "2px solid black" }}
-//         >
-//           Reset
-//         </button>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Option;
-import css from "./Options.module.css";
-
-const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
+function Option({ updateFeedback, resetFeedback }) {
   return (
-    <div className={css.options}>
-      <button className={css.good} onClick={() => updateFeedback("good")}>
-        Good
-      </button>
-      <button className={css.neutral} onClick={() => updateFeedback("neutral")}>
-        Neutral
-      </button>
-      <button className={css.bad} onClick={() => updateFeedback("bad")}>
-        Bad
-      </button>
-      {totalFeedback > 0 && (
-        <button onClick={resetFeedback} className={css.resetButton}>
+    <>
+      <div className='mt-4'>
+        <button
+          onClick={() => updateFeedback("good")}
+          className='btn btn-outline-dark px-3 py-2 m-2'
+          style={{ border: "2px solid black" }}
+        >
+          Good
+        </button>
+        <button
+          onClick={() => updateFeedback("neutral")}
+          className='btn btn-outline-dark px-3 py-2 m-2'
+          style={{ border: "2px solid black" }}
+        >
+          Neutral
+        </button>
+        <button
+          onClick={() => updateFeedback("bad")}
+          className='btn btn-outline-dark px-3 py-2 m-2'
+          style={{ border: "2px solid black" }}
+        >
+          Bad
+        </button>
+        <button
+          onClick={resetFeedback} 
+          className='btn btn-outline-dark px-3 py-2 m-2'
+          style={{ border: "2px solid black" }}
+        >
           Reset
         </button>
-      )}
-    </div>
+      </div>
+    </>
   );
-};
+}
 
-export default Options;
+export default Option; 
