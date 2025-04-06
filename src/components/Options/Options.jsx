05a0004b829e062bdb25f5cdf07e411254a8f1ff
lika_vil <125767690+Lika060@users.noreply.github.com>
2025-04-06 19:31:23 +0300

    Changes to be committed: 	modified:   src/components/Options/Options.jsx

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,40 +1,25 @@
-import css from './Options.module.css';
 
-function Option({ updateFeedback, resetFeedback }) {
+import css from "./Options.module.css";
+
+const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
   return (
-    <>
-      <div className='mt-4'>
-        <button
-          onClick={() => updateFeedback("good")}
-          className='btn btn-outline-dark px-3 py-2 m-2'
-          style={{ border: "2px solid black" }}
-        >
-          Good
-        </button>
-        <button
-          onClick={() => updateFeedback("neutral")}
-          className='btn btn-outline-dark px-3 py-2 m-2'
-          style={{ border: "2px solid black" }}
-        >
-          Neutral
-        </button>
-        <button
-          onClick={() => updateFeedback("bad")}
-          className='btn btn-outline-dark px-3 py-2 m-2'
-          style={{ border: "2px solid black" }}
-        >
-          Bad
-        </button>
-        <button
-          onClick={resetFeedback} 
-          className='btn btn-outline-dark px-3 py-2 m-2'
-          style={{ border: "2px solid black" }}
-        >
+    <div className={css.options}>
+      <button className={css.good} onClick={() => updateFeedback("good")}>
+        Good
+      </button>
+      <button className={css.neutral} onClick={() => updateFeedback("neutral")}>
+        Neutral
+      </button>
+      <button className={css.bad} onClick={() => updateFeedback("bad")}>
+        Bad
+      </button>
+      {totalFeedback > 0 && (
+        <button onClick={resetFeedback} className={css.resetButton}>
           Reset
         </button>
-      </div>
-    </>
+      )}
+    </div>
   );
-}
+};
 
-export default Option; 
+export default Options;
